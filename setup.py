a5769d02562d206edb6e0da3fb48de78c266d7da
stephan_becque <stephan.becque>
2019-07-17 13:52:40 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tetris_python',
+      version='0.1',
+      description='Tetris in python',
+      url='http://github.com/sjbecque/tetris-python',
+      author='sjbecque',
+      author_email='sjbecque@github.com',
+      license='MIT',
+      packages=['tetris'],
+      zip_safe=False)
